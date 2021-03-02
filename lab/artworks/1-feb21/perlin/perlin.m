24976f0ba8db5ab9dc010153962642a0464e1c8f
mariaguell <mariagllmlst@gmail.com>
2021-03-02 19:01:32 +0100

    Ja quasi tinc resolt l'algorisme del perlin...

--- lab/artworks/1-feb21/perlin/perlin.m
+++ lab/artworks/1-feb21/perlin/perlin.m
@@ -54,7 +54,6 @@ for i = 1:s
         y = h * j;
         v = pol2cart(vectorsGrid(i, j), s * .5);
         line([ x, x + v(1)], [y, y + v(2)], 'Color', 'red');
-        #plot(x,y, 'Color', 'red', 'LineWidth', 5);
         hold on
     end
 end
@@ -68,76 +67,62 @@ end
 
 
 
+dotProduct = zeros(n, n);
 
+for x = 1:n
+    i = uint8(round(x/s));
+    for y = 1:n
+        j = uint8(round(y/s));
+        if j == 0; j = 1; end; if i == 0; i = 1; end
+        v = pol2cart(vectorsGrid(i, j), s * .5);
+        gx = i * w;
+        gy = j * h;
+        dx = double(x - gx);
+        dy = double(y - gy);
+        p = [dx dy];
+        dotProduct(x, y) = dot(p,v);
+    end
+end
 
-
-
-x=35; y=68;
-i = uint8(round(x/s));j = uint8(round(y/s));
-
-a = vectorsGrid(i, j);
-v = pol2cart(a, 100);
-r = pol2cart(a + pi *.5, 100);
-
-x2 = w * (i +.5);
-y2 = h * (j +.5);
-
-l1 = createLine([x, x+v(1)], [y, y+v(2)]);
-l2 = createLine([x2,x2+r(1)], [y2,y2+r(2)]);
-p = intersectLines(l1, l2)
-
-figure(2)
-line(l1(1:2),l1(3:4), 'Color', 'red');
-hold on
-line(l2(1:2),l2(3:4), 'Color', 'blue');
-hold on
-plot(p, 'Color', 'green');
-
-
+figure(1)
+subplot(2, 2, 2);
+title('Dot product');
+pcolor(dotProduct);
 
 
 
 
 
-#G = zeros(n, n);
-
-#for x = 1:n
-#    i = uint8(round(x/s));
-#    for y = 1:n
-#        j = uint8(round(y/s));
-#        if j == 0; j = 1; end; if i == 0; i = 1; end
-#        r = vectorsGrid(i, j, :) * rotz;
-#        v = point2Line([x,y,0], [i,j,0], [r(1),r(2),0]);
-#        G(x, y) = i*j;
-#    end
-#end
-
-#figure(1)
-#subplot(2, 2, 2);
-#title('Dot product');
-#pcolor(G);
-
-
-
-
-
-
-
-
-
-
-# ...
+# Interpolation
 # ==================================================
 
 
 
-# subplot(2, 2, 3);
-# title('Interpolació');
+# ...
+# ------------------------------
 
-# imshow(G);
+M = max(max(dotProduct));
+a = dotProduct / M;
+a = a * .5 + .5;
 
 
-# subplot(2, 2, 4);
-# title('');
 
-# imshow(G);
+# ...
+# ------------------------------
+
+F=1/9*ones(n);
+interpolation=filter2(a,F);
+
+
+
+# ...
+# ------------------------------
+
+figure(1)
+subplot(2, 2, 3);
+title('...');
+imshow(a);
+
+subplot(2, 2, 4);
+title('Interpolation');
+imshow(interpolation);
