f989b54375ec24f5f5c7013436506e190438ca96
mariaguell <mariagllmlst@gmail.com>
2021-07-18 20:11:05 +0200

    Web de presentació pràcticament acabada

--- com/CV/sketch.js
+++ com/CV/sketch.js
@@ -1,110 +1,78 @@
-const SYMS = 8;
-const bgColor = 12;
-const bgAplha = .05;
-const fgColor = 255 - bgColor;
-const fgWeight= 5;
-var sun, centerPoint;
+var circleRadius = 100, lineHeight = 200, speed = .02;
+
+var fase = 0, a = 0, dir = 1;
+var faseMax = 4.712388;
+
+var circlePosition;
+var fgColor = [ 180, 60, 50, .1 ];
 
 
 
 function setup( ) {
 	createCanvas( window.innerWidth, window.innerHeight );
 
+	const c = ( width > height ? width : height ) * .05;
+	circleRadius = c, lineHeight = c*2;
+
 	frameRate( 24 );
-
-	colorMode( HSL );
-	stroke( fgColor );
-	strokeWeight( fgWeight );
-	background( bgColor );
-
-	createPlanets();
+	setStyle( );
+	circlePosition = createVector( width * .5, height * .5 );
 }
 
 function draw( ) {
-	const c = color( bgColor );
-	c.setAlpha( bgAplha );
+	//displayBackground( );
+	displayCurrentLine( );
+	updateFase( );
+	fgColor[0] += .1;
+	if ( fgColor[0] > 360 ) fgColor[0] = 0;
+	stroke( fgColor );
+}
+
+
+
+function setStyle( ) {
+	colorMode( HSL );
+	angleMode( RADIANS );
+	background( 12 );
+	strokeWeight( 5 );
+	stroke( fgColor );
+	noFill();
+}
+
+function updateFase( ) {
+	fase += speed;
+	a += speed * dir;
+	if ( fase > faseMax ) changeCircle();
+}
+
+function displayBackground( ) {
+	const c = color( 12 );
+	c.setAlpha( .05 );
 	background( c );
-	sun.update( );
-	drawPointSymmetrically( function(){ sun.display() } );
 }
 
-
-
-function drawPointSymmetrically( drawCallback ) {
-	translate( width * .5, height * .5 );
-	for ( var i = 0; i < SYMS; i++ ) {
-		push( );
-		rotate( TAU * i / SYMS );
-		drawCallback( );
-		pop( );
-	}
+function displayCurrentLine( ) {
+	const p1 = createVector(0, 0); const p2 = createVector(0, 0);
+	p1.x = circlePosition.x + cos( a ) * circleRadius;
+	p1.y = circlePosition.y + sin( a ) * circleRadius;
+	p2.x = circlePosition.x + cos( a ) * ( circleRadius + lineHeight );
+	p2.y = circlePosition.y + sin( a ) * ( circleRadius + lineHeight );
+	line( p1.x, p1.y, p2.x, p2.y );
 }
 
-function createPlanets() {
-	sun = new Planet( 1, 0 );
-
-	const earth = new Planet( 250, .02 );
-	sun.addSatellite( earth );
-
-	const moon = new Planet( 300, .05 );
-	earth.addSatellite( moon );
-
-	const submoon = new Planet( 350, .08 );
-	moon.addSatellite( submoon );
-
-	submoon.setDrawable( );
+function ckeckPointInsideCanvas( p ) {
+	if ( p.x > width ) p.x = p.x - width;
+	else if ( p.x < 0 ) p.x = width + p.x;
+	if ( p.y > height ) p.y = p.y - height;
+	else if ( p.y < 0 ) p.y = height + p.y;
 }
 
-
-
-
-
-class Planet {
-	constructor ( orbitRadius, speed ) {
-		this.orbitCenter = createVector( 0, 0 );
-		this.orbitRadius = orbitRadius;
-		this.speed = speed;
-		this.fase = 0;
-		this.drawable = false;
-		this.satellites = [ ];
-		this.parent = null;
-	}
-
-	addSatellite ( planet ) {
-		this.satellites.push( planet );
-		planet.parent = this;
-	}
-
-	setOrbitCenter ( pos ) { this.orbitCenter = pos; }
-
-	setFase ( f ) { this.fase = f; }
-	
-	setDrawable ( ) { this.drawable = true; }
-
-	get position ( ) {
-		const x = this.orbitCenter.x + cos( this.fase ) * this.orbitRadius * 0.5;
-		const y = this.orbitCenter.y + sin( this.fase ) * this.orbitRadius * 0.5;
-		return createVector( x, y );
-	}
-
-	display ( ) {
-		if ( this.drawable ) {
-			const p1 = this.position;
-			const p2 = this.parent.position;
-			line( p1.x, p1.y, p2.x, p2.y );
-		}
-		this.satellites.forEach( satellite => {
-			satellite.display( );
-		});
-	}
-
-	update () {
-		this.fase += this.speed;
-		if ( this.fase >= TAU ) this.fase = 0;
-		const p = this.position;
-		this.satellites.forEach( satellite => {
-			satellite.setOrbitCenter( p );
-			satellite.update();
-		});
-	}
+function changeCircle( ) {
+	fase = 0;
+	circlePosition.x += cos( a ) * ( circleRadius * 2 + lineHeight ) ;
+	circlePosition.y += sin( a ) * ( circleRadius * 2 + lineHeight ) ;
+	ckeckPointInsideCanvas( circlePosition );
+	faseMax = HALF_PI * floor( random( 1, 4 ) );
+	a -= PI;
+	dir *= -1;
 }
